e164789cb2249ace902a7e7e3431c5294e9a178f
Michael Franklin <michael.franklin@petermac.org>
2020-07-19 10:46:46 +1000

    Update install script to init and download data

--- ec2/02-on-first-load.sh
+++ ec2/02-on-first-load.sh
@@ -3,6 +3,25 @@
 # Start docker service
 sudo service docker start
 
+
+
+# setup Janis with CWLTool
+mkdir -p ~/janis-bcc2020 && cd ~/janis-bcc2020
+
+mkdir -p ~/.janis/
+cat <<EOT >> ~/.janis/janis.conf
+engine: cwltool
+notifications:
+  email: null
+template:
+  id: local
+EOT
+
+# Download data
+
+wget -q -O- "https://github.com/PMCC-BioinformaticsCore/janis-workshops/raw/master/bcc2020/resources/bcc-data.tar" \
+    | tar -xz
+
 # Allow password login
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
